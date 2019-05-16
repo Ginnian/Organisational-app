354ef848565e95d2071b3b8bdfb89c85832e0747
Ginnian <virginia.waikari@gmail.com>
2019-05-16 16:40:15 +1200

    schedule and journal now correctly sending data to home screen. Next commit will work with deleting and updating entries I will also remove all of the old recycler view from the schedule tab as well as classes that used it.

--- app/src/main/java/com/example/sd6501_assignment1_2180511/Schedule/ScheduleFragment.java
+++ app/src/main/java/com/example/sd6501_assignment1_2180511/Schedule/ScheduleFragment.java
@@ -9,19 +9,26 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.sd6501_assignment1_2180511.DatabaseHandlerUsers;
 import com.example.sd6501_assignment1_2180511.DraftScheduleAdapter;
+import com.example.sd6501_assignment1_2180511.Home.MainActivity;
 import com.example.sd6501_assignment1_2180511.R;
 
 import java.util.ArrayList;
 
 public class ScheduleFragment extends Fragment {
+    //debug
+    private static final String TAG = "ScheduleFragment";
+
     private CalendarView calendar;
     private EditText eventEntry;
     private TextView currentDate;
@@ -30,9 +37,9 @@ public class ScheduleFragment extends Fragment {
 //    Build event view
     ArrayList<ScheduleClass> events = new ArrayList<>();
     ScheduleClass eventObj = new ScheduleClass();
-    RecyclerView rv;
-    RecyclerView.Adapter rvAdapter;
-    RecyclerView.LayoutManager rvLayoutManager;
+//    RecyclerView rv;
+//    RecyclerView.Adapter rvAdapter;
+//    RecyclerView.LayoutManager rvLayoutManager;
 
     @Nullable
     @Override
@@ -40,11 +47,17 @@ public class ScheduleFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_schedule, container, false);
 
         findViews(v);
-        buildRecyclerLayout();
+//        buildRecyclerLayout();
+
+        //get session id
+        MainActivity mainActivity = (MainActivity) getActivity();
+        final long userID = mainActivity.getUserID();
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Log.d(TAG, "onSelectedDayChange: Retrieving the date from the calendar");
+
                 currentDate.setText(dayOfMonth + "/" + month + "/" + year);
                 eventObj.setDate(dayOfMonth + "/" + month + "/" + year);
             }
@@ -55,7 +68,11 @@ public class ScheduleFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 eventObj.setEntry(eventEntry.getText().toString().trim());
-                events.add(eventObj);
+                
+                saveJournalToDB(userID);
+                Toast.makeText(getActivity().getApplicationContext(), 
+                        "New schedule added", Toast.LENGTH_SHORT).show();
+//                events.add(eventObj);
                 eventEntry.getText().clear();
             }
         });
@@ -88,14 +105,21 @@ public class ScheduleFragment extends Fragment {
         eventEntry = v.findViewById(R.id.schedule_et_entry);
         currentDate = v.findViewById(R.id.schedule_tv_dateSelected);
         floatingActionButton = v.findViewById(R.id.schedule_fab_addSchedule);
-        rv = v.findViewById(R.id.schedule_rv_allevents);
+//        rv = v.findViewById(R.id.schedule_rv_allevents);
     }  //find view variables
 
-    public void buildRecyclerLayout() {
-        rv.setHasFixedSize(true);
-        rvLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
-        rvAdapter = new DraftScheduleAdapter(events);
-        rv.setLayoutManager(rvLayoutManager);
-        rv.setAdapter(rvAdapter);
-    } //Build event layout for the recycler view
+//    public void buildRecyclerLayout() {
+//        rv.setHasFixedSize(true);
+//        rvLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+//        rvAdapter = new DraftScheduleAdapter(events);
+//        rv.setLayoutManager(rvLayoutManager);
+//        rv.setAdapter(rvAdapter);
+//    } //Build event layout for the recycler view
+
+    private void saveJournalToDB(long userID) {
+        Log.d(TAG, "saveJournalToDB: Saving schedule to database");
+        
+        DatabaseHandlerUsers dbhandler = new DatabaseHandlerUsers(getActivity().getApplicationContext());
+        dbhandler.insertSchedule(userID, eventObj.getEntry(), eventObj.getSubject(), eventObj.getDate());
+    }
 }
